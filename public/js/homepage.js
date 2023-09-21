cae849ce019a07fe532a8df07583849eae45b1cf
matthew <matthkang@gmail.com>
2023-09-20 20:41:45 -0700

    next previous year for cal

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -3,60 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
-        initialDate: '2023-09-07',
         headerToolbar: {
-            left: 'prev,next today',
+            left: 'prevYear,prev,next,nextYear today',
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay'
-        },
-        events: [
-            {
-                title: 'All Day Event',
-                start: '2023-09-01'
-            },
-            {
-                title: 'Long Event',
-                start: '2023-09-07',
-                end: '2023-09-10'
-            },
-            {
-                groupId: '999',
-                title: 'Repeating Event',
-                start: '2023-09-09T16:00:00'
-            },
-            {
-                groupId: '999',
-                title: 'Repeating Event',
-                start: '2023-09-16T16:00:00'
-            },
-            {
-                title: 'Conference',
-                start: '2023-09-11',
-                end: '2023-09-13'
-            },
-            {
-                title: 'Meeting',
-                start: '2023-09-12T10:30:00',
-                end: '2023-09-12T12:30:00'
-            },
-            {
-                title: 'Lunch',
-                start: '2023-09-12T12:00:00'
-            },
-            {
-                title: 'Meeting',
-                start: '2023-09-12T14:30:00'
-            },
-            {
-                title: 'Birthday Party',
-                start: '2023-09-13T07:00:00'
-            },
-            {
-                title: 'Click for Google',
-                url: 'https://google.com/',
-                start: '2023-09-28'
-            }
-        ]
+        }
     });
 
     calendar.render();
