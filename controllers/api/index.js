1a8e3eb99d481c827d6be9687c25c590eea231f2
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-09-21 21:25:30 -0700

    updated file structure

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const userinputRoutes = require('./userinput');
+const calendarRoutes = require('./calendarRoutes');
 
 router.use('/userinput', userinputRoutes);
-
+router.use('/calendar', calendarRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const calendarRoutes = require('./calendarRoutes');
 
-router.use('/', calendarRoutes);
+// router.use('/', calendarRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
