9ab34fe7f07d337061e66268312f1b71e26dc418
matthew <matthkang@gmail.com>
2023-09-21 20:07:17 -0700

    render calendar in homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,30 @@
 const router = require('express').Router();
 const { Event, User } = require('../models');
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  // res.render('homepage');
+  try {
+    // Get all projects and JOIN with user data
+    const eventData = await Event.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const events = eventData.map((event) => event.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      events,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
