f42c5b871d20559165d8ac220ff572870bb3e66c
matthew <matthkang@gmail.com>
2023-09-21 19:03:22 -0700

    remove dupe line

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class User extends Model { }
-class User extends Model { }
 
 User.init(
     {
