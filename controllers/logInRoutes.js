c2ff7d0428de84676a6e4360fb604b284213d0f2
Pixls112 <nguyen.phillip112@gmai.com>
2023-09-24 00:48:37 -0700

    got the login.js to properly work. Need to work on api routes for login page further

--- controllers/logInRoutes.js
+++ controllers/logInRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const User = require('../models/user')
 
 // The `/api/categories` endpoint
 
@@ -8,6 +9,38 @@ router.get('/', async (req, res) => {
     res.render('loginpage');
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ Username });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
